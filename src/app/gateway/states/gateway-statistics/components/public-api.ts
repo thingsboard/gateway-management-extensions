a5898fd89e7e65c74bff77ee3b515e3b372c6dbc
mpetrov <mpetrov@thingsboard.io>
2025-01-10 12:27:16 +0200

    Added implementation of gateway-statistics-chart

--- src/app/gateway/states/gateway-statistics/components/public-api.ts
+++ src/app/gateway/states/gateway-statistics/components/public-api.ts
@@ -16,3 +16,4 @@
 export * from './commands-autocomplete/statistics-commands-autocomplete.component';
 export * from './custom-statistics-table/custom-statistics-table.component';
 export * from './edit-custom-command-dialog/edit-custom-command-dialog.component';
+export * from './chart/gateway-statistics-chart.component';
