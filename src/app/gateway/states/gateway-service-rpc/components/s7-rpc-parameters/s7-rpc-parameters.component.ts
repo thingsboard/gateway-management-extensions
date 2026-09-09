06ce5919c8ec936f38a61cf6c7754b6d69c0dac6
Maksym Tsymbarov <mtsymbarov@thingsboard.io>
2026-09-09 13:16:21 +0200

    Changed Bit validator for S7 connector

--- src/app/gateway/states/gateway-service-rpc/components/s7-rpc-parameters/s7-rpc-parameters.component.ts
+++ src/app/gateway/states/gateway-service-rpc/components/s7-rpc-parameters/s7-rpc-parameters.component.ts
@@ -121,7 +121,7 @@ export class S7RpcParametersComponent extends ControlValueAccessorBaseAbstract<R
       dbNumber: [0, [Validators.required, Validators.min(0)]],
       start: [0, [Validators.required, Validators.min(0)]],
       size: [1, [Validators.required, Validators.min(0)]],
-      bit: [1, [Validators.required, Validators.min(1), Validators.max(7)]],
+      bit: [0, [Validators.required, Validators.min(0), Validators.max(7)]],
       tag: [{value: null, disabled: true}, [Validators.required, Validators.pattern(noLeadTrailSpacesRegex)]],
       vmAddress: [{value: null, disabled: true}, [Validators.required, Validators.pattern(noLeadTrailSpacesRegex)]],
       value: [{value: null, disabled: true}, [Validators.required, Validators.pattern(noLeadTrailSpacesRegex)]],
